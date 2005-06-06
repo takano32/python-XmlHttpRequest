b157753dc055d16fe75041360e3eeaa2979355a4
takano32 <takano32@20b47ea9-8295-4682-bdc9-7f42c2971ab2>
2005-06-06 19:33:14 +0000

    implements basic function
    git-svn-id: file:///home/takano32/.svnrepos/python/trunk@15 20b47ea9-8295-4682-bdc9-7f42c2971ab2

--- client.py
+++ client.py
@@ -1,43 +1,42 @@
+#! /usr/bin/env python
 # Echo client program
 import socket
 
-HOST = 'localhost'    # The remote host
-PORT = 7070              # The same port as used by the server
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((HOST, PORT))
+class SimpleClient:
+  def __init__(self, host, port):
+    self.host = host
+    self.port = port
+    self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-s.send("""
+  def connect(self):
+    opt = (self.host, self.port)
+    self.socket.connect(opt)
+
+  def send(self, message):
+    self.socket.send(message)
+
+  def recv(self):
+    data = list()
+    ch = self.socket.recv(1)
+    while( ch != "\x01" and ch != "\x00"):
+      data.append(ch)
+      ch = self.socket.recv(1)
+    return "".join(data)
+
+  def close(self):
+    self.socket.close()
+
+
+client = SimpleClient('localhost', 7070)
+client.connect()
+print 'Received'
+client.send("""
 <imc>
 <login id="mino01" domain="example.com" pass="mino01"/>
 </imc>\0
 """)
-
-data = s.recv(1024)
-
-s.close()
-print 'Received', repr(data)
-
-
-
-class SimpleClient():
-  def __init__(self, host, port):
-    self.host = host
-	 self.port = port
-	 self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-  def connect():
-	 self.socket.connect(self.host, self.port)
-
-  def send(message):
-    sock = self.socket
-    sock.send(message)
-	 data = list()
-	 while( ch = sock.recv(1)):
-	   data.append(ch)
-	 return "".join(data)
-
-  def close():
-    self.socket.close()
-
+print client.recv()
+client.send("secound step")
+print client.recv()
 
 
