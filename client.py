daf30edbe75331144b1065587c9f27d179f486ab
takano32 <takano32@20b47ea9-8295-4682-bdc9-7f42c2971ab2>
2005-06-06 17:37:22 +0000

    SimpleClient class
    git-svn-id: file:///home/takano32/.svnrepos/python/trunk@12 20b47ea9-8295-4682-bdc9-7f42c2971ab2

--- client.py
+++ client.py
@@ -1,23 +1,30 @@
- # Echo client program
- import socket
+# Echo client program
+import socket
 
- HOST = 'localhost'    # The remote host
- PORT = 7070              # The same port as used by the server
- s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
- s.connect((HOST, PORT))
+HOST = 'localhost'    # The remote host
+PORT = 7070              # The same port as used by the server
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((HOST, PORT))
 
- s.send("""
- <imc>
- <login id="mino01" domain="example.com" pass="mino01"/>
- </imc>\0
- """)
+s.send("""
+<imc>
+<login id="mino01" domain="example.com" pass="mino01"/>
+</imc>\0
+""")
 
- data = s.recv(1024)
+data = s.recv(1024)
 
- s.close()
- print 'Received', repr(data)
+s.close()
+print 'Received', repr(data)
 
 
+
+class SimpleClient():
+  def __init__():
+    pass
+
+  def send(message):
+    pass
 
 
 
